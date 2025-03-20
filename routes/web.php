41fef5a226e4f61d0c54ab5f487e524451cf5bdb
Frima <lyaalii52@gmail.com>
2025-03-20 10:31:13 +0700

    Menambahkan Login View dan Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,40 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ListBarangController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/welcome', function () {  
+    return view('welcome');
+});
+
+Route::get('/user/{id}', function ($id) {  
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+    Route::get('/login', function () {
+        return 'Halaman Login';
+    });
+    
+});
+
+// Route::get('/listbarang/{id}/{nama}', function($id, $nama){
+//    return view('list_barang', compact('id', 'nama'));
+// });
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
+
+Route::get('/login', [LoginController::class, 'index']);
